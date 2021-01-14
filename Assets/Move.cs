c81625f4ca3739cfeecc64a6f0cdf83bda56c78d
Ruda <rudolf.hlavacek@gmail.com>
2021-01-14 23:38:41 +0100

    Reloading.

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEditor;
+using System.Linq;
 
 public class Move : MonoBehaviour
 {
@@ -12,20 +15,21 @@ public class Move : MonoBehaviour
     {
         rb = transform.GetComponent<Rigidbody>(); // získáme komponentu Rigidbody objektu se skriptem
         objectwithAnim = GameObject.FindGameObjectWithTag("Animobject").GetComponent<Animator>(); // metoda FindGameObjectWithTag() vyhledá herní objekt, který má tag = Animobject. Tag našemu objektu nastavíme později v editoru.
+
         Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift) && !objectwithAnim.GetBool("Aim") && !objectwithAnim.GetCurrentAnimatorStateInfo(0).IsName("Inspect")) // pokud držíme W a zároveň levý Shift, nemíříme a není aktivní animace Inspect
         {
-            rb.AddRelativeForce(new Vector3(0, 0, 70)); // přidáme v aktuálním směru hodnotu větší než při chůzi, protože držíme Shift a "běžíme"
-                                                        // Bool Run rozhoduje o tom, jestli se spustí animace běhu nebo ne
+            rb.AddRelativeForce(new Vector3(0, 0, 280 * Time.deltaTime));
             objectwithAnim.SetBool("Run", true); // spustíme animaci běhu změněním bool parametru
             running = true; // náš bool, pomocí kterého pozná náš program, že hráč běží
         }
-        else // pokud se nespustí podmínka výše, nastaví se bool hodnota na false a program ví, že hráč zrovna neběží
+        else // // pokud se nespustí podmínka výše, nastaví se bool hodnota na false a program ví, že hráč zrovna neběží
         {
             objectwithAnim.SetBool("Run", false); // zastaví se animace
             running = false;
@@ -37,27 +41,24 @@ public class Move : MonoBehaviour
                 objectwithAnim.SetBool("Run", false); // zastavujeme animaci běhu a chůze, aby se mohla spustit animace střelby
                 objectwithAnim.SetBool("Shoot", true); // aktivujeme parametry trigger, jako jsou Shoot a Inspect v našem případě
             }
-            if (Input.GetMouseButtonUp(0))
-            {
-                objectwithAnim.SetBool("Shoot", false);
 
-            }
             if (objectwithAnim.GetBool("Shoot"))
             {
-                objectwithAnim.SetBool("Walk", false); // použijeme objekt, který jsme si definovali a získáme z něj komponentu Animator, tím získáme možnost upravovat animace, které jsou k tomuto objektu přichycené
+                objectwithAnim.SetBool("Walk", false); // použijeme objekt, který jsme si definovali, a získáme z něj komponentu Animator, tím získáme možnost upravovat animace, které jsou k tomuto objektu přichycené
             }
             if (Input.GetKey(KeyCode.D)) // pokud někdo stiskne klávesu W, spustí se tato podmínka, dokud bude klávesa držena
             {
-                rb.AddRelativeForce(new Vector3(50, 0, 0)); // na proměnnou přešly vlastnosti a metody komponenty Rigidbody.
-                                                            // metoda AddForce() přidá sílu do určitého směru, který nastavujeme pomocí os x y z.
+                rb.AddRelativeForce(new Vector3(200 * Time.deltaTime, 0, 0)); // na proměnnou přešly vlastnosti a metody komponenty Rigidbody.
+                                                                              // metoda AddForce() přidá sílu do určitého směru, který nastavujeme pomocí os x y z.
                 if (!objectwithAnim.GetBool("Shoot"))
                 {
                     objectwithAnim.SetBool("Walk", true); // pomocí animátoru získáme možnost upravovat animace, které jsou k tomuto objektu přichycené.
-                }                                                            // jednou z funkcí komponenty Animator je změnit hodnotu parametru
+                }                                         // jednou z funkcí komponenty Animator je změnit hodnotu parametru
             }
             if (Input.GetKey(KeyCode.A))
             {
-                rb.AddRelativeForce(new Vector3(-50, 0, 0));
+                rb.AddRelativeForce(new Vector3(-200 * Time.deltaTime, 0, 0));
+
                 if (!objectwithAnim.GetBool("Shoot"))
                 {
                     objectwithAnim.SetBool("Walk", true);
@@ -65,32 +66,32 @@ public class Move : MonoBehaviour
             }
             if (Input.GetKey(KeyCode.W))
             {
-                rb.AddRelativeForce(new Vector3(0, 0, 50));
+                rb.AddRelativeForce(new Vector3(0, 0, 200 * Time.deltaTime));
+
                 if (!objectwithAnim.GetBool("Shoot"))
                 {
                     objectwithAnim.SetBool("Walk", true);
                 }
-
             }
             if (Input.GetKey(KeyCode.S))
             {
-                rb.AddRelativeForce(new Vector3(0, 0, -50));
+                rb.AddRelativeForce(new Vector3(0, 0, -200 * Time.deltaTime));
                 if (!objectwithAnim.GetBool("Shoot"))
                 {
                     objectwithAnim.SetBool("Walk", true);
                 }
             }
-            if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D)) // pokud pustíme W a S nebo D, tak se spustí tato podmínka
+            if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))// pokud pustíme W a S nebo D, tak se spustí tato podmínka
             {
                 objectwithAnim.SetBool("Walk", false);
             }
-            if (Input.GetMouseButton(1)) // pravé tlačítko (označení 1)
+            if (Input.GetMouseButton(1) && !objectwithAnim.GetCurrentAnimatorStateInfo(0).IsName("Reload Ammo Left") && !objectwithAnim.GetCurrentAnimatorStateInfo(0).IsName("Reload Out Of Ammo")) // pokud dostane Input signál, že se stisklo pravé tlačítko (označení číslo 1)
             {
-                objectwithAnim.SetBool("Aim", true); // nastaví hodnotu parametru Aim
+                objectwithAnim.SetBool("Aim", true); // nastaví hodnotu parametru Aim typu bool na true
             }
             else
             {
-                objectwithAnim.SetBool("Aim", false); // nastaví hodnotu parametru Aim typu bool na false
+                objectwithAnim.SetBool("Aim", false);
             }
             if (Input.GetKeyDown(KeyCode.T))
             {
--- Assets/shoot.cs
+++ Assets/shoot.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
@@ -7,29 +9,35 @@ using UnityEngine.UI;
 public class shoot : MonoBehaviour
 {
     RaycastHit hitInfo;
-    Animator objectwithAnim;
+    Animator objectwithAnim; // hráč
     public float dostrel;
-    public float sila;
+    public float sila; // udělovaná síla Rigidbody
     public float poskozeni;
-    public GameObject efekttrefy;
+    public GameObject efekttrefy; // impact
     Image crosshair;
-    ParticleSystem effekt;
-
+    ParticleSystem effekt; // výstřel
+    Text nabojeText;
+    public int naboje;
+    int maxnaboje;
+    bool reloading;
     void Start()
     {
         objectwithAnim = GameObject.FindGameObjectWithTag("Animobject").GetComponent<Animator>();
         crosshair = GameObject.FindGameObjectWithTag("crosshair").GetComponent<Image>();
         effekt = GameObject.FindGameObjectWithTag("effekt").GetComponent<ParticleSystem>();
+        nabojeText = GameObject.FindGameObjectWithTag("naboje").GetComponent<Text>();
+        maxnaboje = naboje;
+        nabojeText.text = naboje.ToString() + "/" + maxnaboje.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && !objectwithAnim.GetBool("Run") && !objectwithAnim.GetBool("Holster") && !objectwithAnim.GetCurrentAnimatorStateInfo(0).IsName("Inspect")) // pokud zmáčkneme levé tlačítko a neběžíme
+        if (Input.GetMouseButtonDown(0) && !objectwithAnim.GetBool("Run") && !objectwithAnim.GetBool("Holster") && !objectwithAnim.GetCurrentAnimatorStateInfo(0).IsName("Inspect") && naboje > 0 && !reloading) // pokud stiskneme levé tlačítko a neběžíme
         {
-            InvokeRepeating("Shoot", 0, 0.25f); // opakuje metodu shoot, začíná okamžitě bez prodlevy, opakuje se každých 0.25 sekundy
+            InvokeRepeating("Shoot", 0, 0.25f); // opakuje metodu Shoot(), začíná okamžitě bez prodlevy, opakuje se každých 0.25 sekundy
         }
-        if (objectwithAnim.GetBool("Aim"))
+        if (objectwithAnim.GetBool("Aim") || objectwithAnim.GetBool("Holster"))
         {
             crosshair.enabled = false; // změní hodnotu na false
         }
@@ -37,14 +45,27 @@ public class shoot : MonoBehaviour
         {
             crosshair.enabled = true;
         }
+        if (naboje <= 0 || reloading)
+        {
+            CancelInvoke("Shoot");
+            objectwithAnim.SetBool("Shoot", false);
+        }
         if (Input.GetMouseButtonUp(0))
         {
             CancelInvoke("Shoot"); // přestává se volat metoda Shoot()
+            objectwithAnim.SetBool("Shoot", false);
         }
-        if (effekt.isPlaying && effekt.time >= 0.15f) // pokud efekt výstřelu pořád hraje a je
-                                                      // to delší doba než 15 setin sekundy, tak se efekt zastaví
+        if (effekt.isPlaying && effekt.time >= 0.15f)
         {
             effekt.Stop();
+        }
+        if (objectwithAnim.GetCurrentAnimatorStateInfo(0).IsName("Reload Ammo Left") || objectwithAnim.GetCurrentAnimatorStateInfo(0).IsName("Reload Out Of Ammo"))
+        {
+            CancelInvoke("Shoot");
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Reload());
         }
     }
 
@@ -52,8 +73,10 @@ public class shoot : MonoBehaviour
     {
         effekt.Stop();
         effekt.Play();
-        transform.GetComponent<AudioSource>().Stop(); // kdyby byl spuštěn ještě předchozí výstřel, tak ho zastaví, takto nedojde k tomu, že uslyšíme 15 výstřelů najednou
+        transform.GetComponent<AudioSource>().Stop(); // kdyby byl spuštěn ještě předchozí výstřel, tak se zastaví, aby nedošlo k tomu, že uslyšíme 15 výstřelů najednou
         transform.GetComponent<AudioSource>().Play(); // spustí zvuk, který už je
+        naboje -= 1;
+        nabojeText.text = naboje.ToString() + "/" + maxnaboje.ToString();
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo, dostrel)) // pokud něco trefíme, parametry jsou: místo, odkud půjde polopřímka, směr, kam má ukládat informace o tom, co se zasáhlo, dálka, kam až povede
         {
             GameObject trefa = Instantiate(efekttrefy, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
@@ -67,5 +90,29 @@ public class shoot : MonoBehaviour
                 hitInfo.transform.GetComponent<EnemyHealth>().GetDammage(poskozeni); // spouštíme metodu v jiném skriptu, parametrem jsme si nastavili množství poškození
             }
         }
+
     }
+
+    IEnumerator Reload() // typ metody, ve které můžeme používat WaitUntil(zastaví metodu, dokud něco neplatí)
+    {
+        reloading = true;
+        if (naboje > 0)
+        {
+            objectwithAnim.SetTrigger("Reload");
+            yield return new WaitUntil(() => objectwithAnim.GetCurrentAnimatorStateInfo(0).IsName("Reload Ammo Left")); // čeká, dokud nedostane hodnotu true od animátoru, že se přehrává animace
+            yield return new WaitUntil(() => !objectwithAnim.GetCurrentAnimatorStateInfo(0).IsName("Reload Ammo Left")); // čeká, dokud nedostane hodnotu false od animátoru, že se už nepřehrává animace
+            naboje = maxnaboje;
+
+        }
+        else
+        {
+            objectwithAnim.SetTrigger("ReloadNoAmmo");
+            yield return new WaitUntil(() => objectwithAnim.GetCurrentAnimatorStateInfo(0).IsName("Reload Out Of Ammo"));
+            yield return new WaitUntil(() => !objectwithAnim.GetCurrentAnimatorStateInfo(0).IsName("Reload Out Of Ammo"));
+            naboje = maxnaboje;
+        }
+        nabojeText.text = naboje.ToString() + "/" + maxnaboje.ToString();
+        reloading = false;
+    }
+
 }
